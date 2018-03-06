564b6c5ee4745687a0b2a7b6141cf041601cf90a
Miloš Rašić <mrasic@gogoair.com>
2018-03-06 17:39:55 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+"use strict"
+
+const validateOptions = require("schema-utils");
+
+class CustomModuleIdsPlugin {
+	constructor(options) {
+		this.options = options || {};
+
+		validateOptions({
+			type: 'object',
+			additionalProperties: false,
+			properties: {
+				idFunction: {
+					description: "Function called to generate ID of each module. Return value is used as module ID.",
+					instanceof: "Function",
+				},
+			},
+			required: ['idFunction'],
+		}, this.options, "Custom Module IDs Plugin");
+	}
+
+	_processModules(modules, context) {
+		for (const module of modules) {
+			if (module.id === null && module.libIdent) {
+				module.id = this.options.idFunction(module.libIdent({
+					context,
+				}), module);
+			}
+		}
+	}
+
+	apply(compiler) {
+		const context = this.options.context || compiler.options.context;
+
+		if (compiler.hooks) {
+			compiler.hooks.compilation.tap("CustomModuleIdsPlugin", compilation => {
+				compilation.hooks.beforeModuleIds.tap("CustomModuleIdsPlugin", modules => {
+					this._processModules(modules, context);
+				});
+			});
+		}
+		else {
+			compiler.plugin("compilation", compilation => {
+				compilation.plugin("before-module-ids", modules => {
+					this._processModules(modules, context);
+				});
+			});
+		}
+	}
+}
+
+module.exports = CustomModuleIdsPlugin;
